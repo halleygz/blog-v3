2ed34f59bfe1923a873e73c24863f9a08334d07c
Emanah19 <zb69726@gmail.com>
2024-08-10 02:56:21 +0100

    AddBlog page

--- frontend/src/pages/blog/AddBlog.jsx
+++ frontend/src/pages/blog/AddBlog.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import InputFields from '../../components/tools/InputFields'
+import TextArea from '../../components/tools/TextArea'
+import { FormBtns } from '../../components/tools/Buttons'
+import { Link } from 'react-router-dom'
+
+
+
 
 const AddBlog = () => {
   return (
-    <div>AddBlog</div>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-whitesmoke p-6">
+      <div className="w-full max-w-md bg-whitesmoke rounded-lg p-8 md:flex md:max-w-full">
+        <div className="min-h-[100vh] hidden md:flex md:justify-center md:items-center md:w-1/3 bg-[#E6B9A6] text-[#2F3645] p-10 rounded-l-lg">
+          <h2 className="text-[64px] font-bold">Try Buddy</h2>
+        </div>
+        <div className="md:w-1/2 pl-[70px] py-[150px]">
+          <h3 className="text-4xl font-bold font-dm-serif-display text-gray-800 mb-[50px]">What’s in your mind today?</h3>          
+          <form>
+            <InputFields content={"Title"} type={"text"}/>
+            <TextArea content="Content"/>
+          </form>
+          <div className='flex flex-row justify-between'>
+            <div className="max-w-[163px] h-[46px] flex-1 relative box-border border-[1px] border-solid border-gray-300 bg-[#E6B9A6] text-[#2F3645] font-bold mr-4 text-center">Add Tag</div>
+            <input className=" h-[46px] flex-1 relative box-border border-[1px] border-solid border-gray-300 bg-whitesmoke"/>
+          </div> 
+          <div className='my-[40px] mx-[200px]'><FormBtns content={"POST"}/></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
