0975ae90d4e4ea74072fad457143471c310c83b6
Emanah19 <zb69726@gmail.com>
2024-08-10 03:02:48 +0100

    AddBlog modified

--- frontend/src/pages/blog/AddBlog.jsx
+++ frontend/src/pages/blog/AddBlog.jsx
@@ -11,10 +11,10 @@ const AddBlog = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-whitesmoke p-6">
       <div className="w-full max-w-md bg-whitesmoke rounded-lg p-8 md:flex md:max-w-full">
-        <div className="min-h-[100vh] hidden md:flex md:justify-center md:items-center md:w-1/3 bg-[#E6B9A6] text-[#2F3645] p-10 rounded-l-lg">
+        <div className="min-h-[70vh] hidden md:flex md:justify-center md:items-center md:w-1/3 bg-[#E6B9A6] text-[#2F3645] p-10 rounded-l-lg">
           <h2 className="text-[64px] font-bold">Try Buddy</h2>
         </div>
-        <div className="md:w-1/2 pl-[70px] py-[150px]">
+        <div className="md:w-1/2 p-8 pl-[70px]">
           <h3 className="text-4xl font-bold font-dm-serif-display text-gray-800 mb-[50px]">What’s in your mind today?</h3>          
           <form>
             <InputFields content={"Title"} type={"text"}/>
@@ -24,7 +24,7 @@ const AddBlog = () => {
             <div className="max-w-[163px] h-[46px] flex-1 relative box-border border-[1px] border-solid border-gray-300 bg-[#E6B9A6] text-[#2F3645] font-bold mr-4 text-center">Add Tag</div>
             <input className=" h-[46px] flex-1 relative box-border border-[1px] border-solid border-gray-300 bg-whitesmoke"/>
           </div> 
-          <div className='my-[40px] mx-[200px]'><FormBtns content={"POST"}/></div>
+          <div className='my-[40px] mx-[70px]'><FormBtns content={"POST"}/></div>
         </div>
       </div>
     </div>
