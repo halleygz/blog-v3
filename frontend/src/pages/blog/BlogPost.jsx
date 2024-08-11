214f09ba319f1dcb75bb4e95b474a13e80f7df43
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-11 19:29:10 +0300

    test3

--- frontend/src/pages/blog/BlogPost.jsx
+++ frontend/src/pages/blog/BlogPost.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import NavBar from '../../components/tools/NavBar';
 import BlogPostComp from '../../components/blog/BlogPost';
 import Comments from '../../components/blog/Comments';
@@ -8,26 +8,40 @@ import { Link } from 'react-router-dom';
 import { LikeDislike } from '../../components/tools/Buttons';
 
 const BlogPost = () => {
-  const {loading, blog} = useGetBlog()
-  const {authUser} = useAuthContext()
-  const placeHolderLink = !loading && blog.author && authUser.userName === blog.author ? (
-    <Link to={`/edit/${blog._id}`}>
-      <LikeDislike content={"Edit Blog"} />
-    </Link>
-  ) : (
-    <></>
-  );
-  console.log(blog.author)
+  const { loading, blog } = useGetBlog();
+  const { authUser } = useAuthContext();
+  const [placeHolderLink, setPlaceHolderLink] = useState(null);
+
+  useEffect(() => {
+    if (!loading && blog?.author && authUser?.userName === blog.author) {
+      setPlaceHolderLink(
+        <Link to={`/edit/${blog._id}`}>
+          <LikeDislike content={"Edit Blog"} />
+        </Link>
+      );
+    } else {
+      setPlaceHolderLink(null); // Clear the placeholder if conditions don't match
+    }
+  }, [loading, blog, authUser]);
+
+  console.log(blog.author);
+
   return (
     <div className="min-h-screen bg-gray-50 p-4 md:p-8">
-      <NavBar/>
+      <NavBar />
 
       <main className="bg-gray-50 max-w-3xl mx-auto p-4 rounded-md">
-        <BlogPostComp blogData={blog} placeHolder={placeHolderLink}/>
-        <Comments />
+        {!loading && blog ? (
+          <>
+            <BlogPostComp blogData={blog} placeHolder={placeHolderLink} />
+            <Comments />
+          </>
+        ) : (
+          <p>Loading...</p> // Optionally, you can show a loading indicator
+        )}
       </main>
     </div>
   );
-}
+};
 
-export default BlogPost
+export default BlogPost;
