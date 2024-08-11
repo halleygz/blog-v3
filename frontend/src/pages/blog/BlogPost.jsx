5e47519b2dbd11af9b07adbf92c5607837e17651
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-11 19:12:41 +0300

    test2

--- frontend/src/pages/blog/BlogPost.jsx
+++ frontend/src/pages/blog/BlogPost.jsx
@@ -11,16 +11,7 @@ const BlogPost = () => {
   const {loading, blog} = useGetBlog()
   const {authUser} = useAuthContext()
   const placeHolderLink = authUser.userName === blog.author?<Link to={`/edit/${blog._id}`}><LikeDislike content={"Edit Blog"}/></Link>:<></>
-  const someJ = {
-    title: "15 Disadvantages Of Freedom And How You Can Workaround It.",
-    author: "samurai2099",
-    date: "27 may 2022",
-    content: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Luctus venenatis lectus magna fringilla urna. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Elit sed quam 
-      adipiscing vitae proin sagittis nisl rhoncus. Faucibus ornare suspendisse sed nisi lacus sed viverra tellus.
-    `
-}
+  console.log(blog.author)
   return (
     <div className="min-h-screen bg-gray-50 p-4 md:p-8">
       <NavBar/>
