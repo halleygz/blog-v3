0aa32a3cdf8b036d6113c79a3b606adc00d67cf3
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-14 09:08:22 +0300

    color theme updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,8 @@ export default {
     extend: {
       "colors": {
         "whitesmoke": "#eeeded",
+        "pink-300": "#e6b9a6",
+        "pink-400": "#f3a786"
       },
       "fontFamily": {
         "lexend-deca": "'Lexend Deca'",
